916758d2dca6b6daf67c15499606f234e66f1b85
Alejandro Sanchez <aleedario1995@gmail.com>
2025-04-22 15:24:30 -0300

    hora 2

--- pages/api/clicks.js
+++ pages/api/clicks.js
@@ -7,33 +7,21 @@ export default async function handler(req, res) {
   const { start, end } = req.query;
   console.log('Parámetros recibidos del frontend:', { start, end });
 
-  // Obtenemos las fechas pasadas como parámetro, si no están presentes, tomamos la fecha actual
+  // Convertimos las fechas a objetos Date
   const startDate = start ? new Date(start) : new Date();
   const endDate = end ? new Date(end) : new Date(startDate);
 
-  // Convertimos las fechas a la zona horaria de Argentina (GMT-3)
-  const argentinaOffset = -3 * 60; // Offset para GMT-3 en minutos
+  // Convertimos las fechas a UTC (MongoDB las guarda en UTC)
+  const startDateUTC = new Date(Date.UTC(startDate.getUTCFullYear(), startDate.getUTCMonth(), startDate.getUTCDate(), 0, 0, 0));
+  const endDateUTC = new Date(Date.UTC(endDate.getUTCFullYear(), endDate.getUTCMonth(), endDate.getUTCDate(), 23, 59, 59, 999));
 
-  // Ajustamos las fechas a la zona horaria de Argentina
-  const startDateInArgentina = new Date(startDate);
-  startDateInArgentina.setMinutes(startDateInArgentina.getMinutes() + argentinaOffset);
-
-  const endDateInArgentina = new Date(endDate);
-  endDateInArgentina.setMinutes(endDateInArgentina.getMinutes() + argentinaOffset);
-
-  // Definimos el inicio y fin del día en la zona horaria de Argentina
-  const startOfDay = new Date(startDateInArgentina.setHours(0, 0, 0, 0));
-  const endOfDay = new Date(endDateInArgentina.setHours(23, 59, 59, 999));
-
-  // Convertimos las fechas al formato ISO en UTC para asegurar que la base de datos pueda hacer la comparación correctamente
-  const startOfDayISO = startOfDay.toISOString();
-  const endOfDayISO = endOfDay.toISOString();
+  console.log('Fechas convertidas a UTC:', { startDateUTC, endDateUTC });
 
   // Realizamos la consulta a la base de datos filtrando por fecha
   const clicks = await Click.find({
     createdAt: {
-      $gte: startOfDayISO,
-      $lte: endOfDayISO
+      $gte: startDateUTC,
+      $lte: endDateUTC
     }
   })
     .sort({ createdAt: -1 })
