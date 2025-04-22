69aa93ffbd5997cc20db31b2399daed7fb8455d9
Alejandro Sanchez <aleedario1995@gmail.com>
2025-04-22 11:04:22 -0300

    Estilos 8.0

--- pages/api/clicks.js
+++ pages/api/clicks.js
@@ -8,7 +8,11 @@ export default async function handler(req, res) {
   const filter = {};
   if (start || end) {
     filter.createdAt = {};
-    if (start) filter.createdAt.$gte = new Date(start);
+    if (start) {
+      const startDate = new Date(start);
+      startDate.setHours(0, 0, 0, 0); // inicio del día
+      filter.createdAt.$gte = startDate;
+    }
     if (end) {
       const endDate = new Date(end);
       endDate.setHours(23, 59, 59, 999);
