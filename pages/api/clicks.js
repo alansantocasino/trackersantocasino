bcf3fed3c1d7afe70437dd3b1a548b43b726f3bc
Alejandro Sanchez <aleedario1995@gmail.com>
2025-04-22 14:50:24 -0300

    Estilos 14

--- pages/api/clicks.js
+++ pages/api/clicks.js
@@ -6,12 +6,25 @@ export default async function handler(req, res) {
 
   const { start, end } = req.query;
 
+  // Obtenemos las fechas pasadas como parámetro, si no están presentes, tomamos la fecha actual
   const startDate = start ? new Date(start) : new Date();
   const endDate = end ? new Date(end) : new Date(startDate);
 
-  const startOfDay = new Date(startDate.setHours(0, 0, 0, 0));
-  const endOfDay = new Date(endDate.setHours(23, 59, 59, 999));
+  // Convertimos las fechas a la zona horaria de Argentina (GMT-3)
+  const argentinaOffset = -3 * 60; // Offset para GMT-3 en minutos
 
+  // Ajustamos las fechas a la zona horaria de Argentina
+  const startDateInArgentina = new Date(startDate);
+  startDateInArgentina.setMinutes(startDateInArgentina.getMinutes() + argentinaOffset);
+
+  const endDateInArgentina = new Date(endDate);
+  endDateInArgentina.setMinutes(endDateInArgentina.getMinutes() + argentinaOffset);
+
+  // Definimos el inicio y fin del día en la zona horaria de Argentina
+  const startOfDay = new Date(startDateInArgentina.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(endDateInArgentina.setHours(23, 59, 59, 999));
+
+  // Realizamos la consulta a la base de datos filtrando por fecha
   const clicks = await Click.find({
     createdAt: {
       $gte: startOfDay,
